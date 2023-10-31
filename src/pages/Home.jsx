b567f61fd47033e03efe5ad5bfc334378b01a7f4
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-11-01 00:19:33 +0530

    added dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
-    <div className="px-10 py-5 bg-gray-50 h-[200vh]">
+    <div className="px-10 py-5 bg-gray-50 h-[200vh] dark:bg-slate-900">
       <header className="">
         <NavBar />
       </header>
--- src/ui/Switch.jsx
+++ src/ui/Switch.jsx
@@ -3,6 +3,14 @@ import { BsMoon, BsSun } from "react-icons/bs";
 import React, { useState } from "react";
 export default function Switch() {
   const [theme, setTheme] = useState("dark");
+  if (theme === "dark") {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+  console.log(theme);
   return (
     <button
       role="button"
